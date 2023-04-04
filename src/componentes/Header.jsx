96a2ec1462c6dd716be9096adf938f5b2a4676d3
VictorSilva244 <victor.silva37@outlook.com>
2023-04-03 23:04:16 -0300

    Atividade 03/04

---
+++ src/componentes/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+
+export function Header(){
+    return(
+        <div className='Header'>
+            <h1>Victor Barbosa da Silva</h1>
+            <h1>060963</h1>
+        </div>
+    )
+}
